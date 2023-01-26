e915e3e1bf2a0637e051b69e199714a3e9155ef9
gustavouliano <gustavouliano11@gmail.com>
2023-01-25 23:44:56 -0300

    Change SendForgotPasswordEmailService to use AWS-SES

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -1,9 +1,11 @@
-import EtherealMail from "@config/mail/EtherealMail";
 import AppError from "@shared/errors/AppError";
 import { getCustomRepository } from "typeorm";
 import path from 'path';
 import UsersRepository from "../typeorm/repositories/UsersRepository";
 import UserTokensRepository from "../typeorm/repositories/UserTokensRepository";
+import EtherealMail from "@config/mail/EtherealMail";
+import SESMail from "@config/mail/SESMail";
+import mailConfig from '@config/mail/mail';
 
 interface IRequest {
     email: string;
@@ -23,19 +25,37 @@ export default class SendForgotPasswordEmailService {
         const userToken = await userTokensRepository.generate(user.id);
         const forgotPasswordTemplate = path.resolve(__dirname, '..', 'views', 'forgot_password.hbs');
 
-        await EtherealMail.sendMail({
-            to: {
-                name: user.name,
-                email: user.email
-            },  
-            subject: '[API Vendas] Recuperação de Senha',
-            templateData: {
-                file: forgotPasswordTemplate,
-                variables: {
+        if(mailConfig.driver == 'ses'){
+            await SESMail.sendMail({
+                to: {
                     name: user.name,
-                    link: `${process.env.APP_WEB_URL}/reset_password?token=${userToken.token}`
+                    email: user.email
+                },  
+                subject: '[API Vendas] Recuperação de Senha',
+                templateData: {
+                    file: forgotPasswordTemplate,
+                    variables: {
+                        name: user.name,
+                        link: `${process.env.APP_WEB_URL}/reset_password?token=${userToken.token}`
+                    }
                 }
-            }
-        });
+            });
+        }
+        else{
+            await EtherealMail.sendMail({
+                to: {
+                    name: user.name,
+                    email: user.email
+                },  
+                subject: '[API Vendas] Recuperação de Senha',
+                templateData: {
+                    file: forgotPasswordTemplate,
+                    variables: {
+                        name: user.name,
+                        link: `${process.env.APP_WEB_URL}/reset_password?token=${userToken.token}`
+                    }
+                }
+            });
+        }
     }
 }
